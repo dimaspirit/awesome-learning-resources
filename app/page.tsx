2ef08ba4f69839d81076b58c347b1b8aa7b2eba7
Dmytro Lobanov <dspwww@gmail.com>
2025-02-27 17:36:56 +0000

    Finalized v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import styles from "./page.module.css";
+import resources from '../data/resources';
 
 export default function Home() {
+
   return (
     <div className={styles.page}>
-     <h1>Hello</h1>
+     <h1>Awesome learning resources</h1>
+
+     <div>
+      {resources.map((resource) => (
+        <div key={resource.title} className={styles.resource}>
+          <h2>{resource.title}</h2>
+          <a href={resource.link} target="_blank">Link</a>
+          <p>Type: {resource.type}</p>
+          <p>Certification: {resource.certification.join(', ')}</p>
+          <p>Languages: {resource.languages.join(', ')}</p>
+          <p>Level: {resource.level}</p>
+          <p>LPrice: {resource.price}</p>
+        </div>
+      ))}
+     </div>
     </div>
   );
 }
---
+++ data/resources.ts
@@ -0,0 +1,40 @@
+const resources = [
+  {
+    "link": "https://www.freecodecamp.org/learn/",
+    "type": "courses",
+    "certification": ["certification"],
+    "title": "freeCodeCamp",
+    "languages": ["English"],
+    "level": "All levels",
+    "price": "Free"
+  },
+  {
+    "link": "https://fullstackopen.com/",
+    "type": "path",
+    "certification": ["certification"],
+    "title": "FullStackOpen",
+    "languages": ["English", "Finnish", "Chinese", "Spanish", "French", "Portuguese"],
+    "level": "All levels",
+    "price": "Free"
+  },
+  {
+    "link": "https://prometheus.org.ua/courses-catalog?price=0",
+    "type": "course",
+    "certification": ["certification"],
+    "title": "Prometheus",
+    "languages": ["Ukrainian"],
+    "level": "All levels",
+    "price": "Freemium"
+  },
+  {
+    "link": "https://developer.mozilla.org/en-US/curriculum/",
+    "type": "path",
+    "certification": ["certification"],
+    "title": "MDN Curriculum",
+    "languages": ["Ukrainian"],
+    "level": "beginner",
+    "price": "Free"
+  }
+]
+
+export default resources;
