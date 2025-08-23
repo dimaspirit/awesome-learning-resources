ebb1c3a80961ae9dfec6bfecb0efd0a4318bf96a
Dmytro Lobanov <dspwww@gmail.com>
2025-08-23 17:42:20 +0100

    fix(eslint): Add components/ui to ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat, globalIgnores } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  globalIgnores([
+    "/components/ui/*"
+  ])
 ];
 
 export default eslintConfig;
